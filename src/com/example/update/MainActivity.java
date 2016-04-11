018f4fc161c2a0e7c505478728e8319f1d87890e
ArryLuo <1394465750@qq.com>
2016-04-11 16:00:21 +0800

    对图片进行压缩
    根据图片的路径对图片进行压缩

--- src/com/example/update/MainActivity.java
+++ src/com/example/update/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.update;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -41,13 +46,45 @@ public class MainActivity extends Activity {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == RESULT_OK && requestCode == 100) {
 			if (data != null) {
-				img.setImageURI(data.getData());
+				// img.setImageURI(data.getData());
 				ContentResolver resolver = getContentResolver();
 				// 照片的原始资源地址
 				Uri originalUri = data.getData();
 				Toast.makeText(MainActivity.this, originalUri + "", 1).show();
 				String path = getPath(originalUri);
-				Log.v("TAG", path + "图片的路径");
+				// 使用ContentProvider通过URI获取原始图片
+
+				try {
+					Bitmap photo = MediaStore.Images.Media.getBitmap(resolver,
+							originalUri);
+					BitmapFactory.Options options = new BitmapFactory.Options();
+					options.inJustDecodeBounds = true;
+					// 获取这个图片的宽和高
+					Bitmap bitmap = BitmapFactory.decodeFile(path, options); // 此时返回bm为空
+					options.inJustDecodeBounds = false;
+					int heig = options.outHeight;
+					Log.v("TAG", heig + "图片高度");
+					// 计算缩放比
+					int be = (int) (options.outHeight / (float) 200);
+					if (be <= 0)
+						be = 1;
+					options.inSampleSize = be;
+					// 重新读入图片，注意这次要把options.inJustDecodeBounds 设为 false哦
+					bitmap = BitmapFactory.decodeFile(path, options);
+					int w = bitmap.getWidth();
+					Log.v("TAG", w + "图片高度");
+					int h = bitmap.getHeight();
+					Log.v("TAG", h + "图片高度");
+					System.out.println(w + "   " + h);
+					img.setImageBitmap(bitmap);
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 
 		}
